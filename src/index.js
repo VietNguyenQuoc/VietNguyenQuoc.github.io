0cec6aa1ef15a1c33256edf50fbc21d35efb6648
VietNguyenQuoc <nqviet157@gmail.com>
2022-06-05 15:27:33 +0700

    tut: net ninja tailwind

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(createElement("h1", {}, "Hello Viet"));
+
+const App = () => {
+    return <>
+        <h1 onClick={() => alert(1)}></h1>
+    </>
+}
